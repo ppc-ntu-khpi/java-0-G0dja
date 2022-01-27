14d12ff924ffbc1f6b5b2575688677e9817f5fd0
G0dja <91980285+G0dja@users.noreply.github.com>
2022-01-27 07:44:24 +0000

    t

--- src/Shirt.java
+++ src/Shirt.java
@@ -1,9 +1,9 @@
 public class Shirt {
-  public int shirtID = 0; // стандартне значення номера моделі сорочки
-  public String description = "-description required-"; // стандартний опис сорочки
+  public int shirtID = 35; // стандартне значення номера моделі сорочки
+  public String description = "Без рукавів і глютена"; // стандартний опис сорочки
   // коди кольорів: R=червоний, B=синій, G=зелений, U=невідомо
-  public char colorCode = 'U';
-  public double price = 0.0; // стандартна вартість сорочки
+  public String colorCode = "red";
+  public double price = 349.99; // стандартна вартість сорочки
   public int quantityInStock = 0; // стандартна кількість на складі
   
   // цей метод просто виводить всю інформацію про сорочку на екран
